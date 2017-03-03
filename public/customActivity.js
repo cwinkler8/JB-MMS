39070d5a3dba9348026ea7edc4874db1769ed467
anthony1993 <dotson.anthony@gmail.com>
2017-03-02 21:56:31 -0800

    adding in a shim for underscore

--- public/customActivity.js
+++ public/customActivity.js
@@ -1,10 +1,20 @@
 requirejs.config({
+  shim: {
+    underscore: {
+      exports: '_'
+    },
+    backbone: {
+      deps: ["underscore", "jquery"],
+      exports: "Backbone"
+    }
+    },    
     paths: {
         postmonger: 'js/postmonger'
     }
 });
 
-var _ = require('underscore');
+//the "main" function to bootstrap your code
+require(['jquery', 'underscore', 'backbone'], function ($, _, Backbone) {   // or, you could use these deps in a separate module using define
 
 var t;
 var lastRowNum = 0;
